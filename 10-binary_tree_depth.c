a104aa32e59a47c01568ea16effb0974df0ed99e
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-12-13 01:53:38 -0800

    binary_trees

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - depth of a node in a binary tree
+ * @tree: pointer to the node to measure the depth
+ * Return: Depth of the node, tree
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree && tree->parent)
+	{
+		depth = binary_tree_depth(tree->parent) + 1;
+	}
+
+	return (depth);
+}
---
+++ 9-binary_tree_height.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+#include <math.h>
+
+/**
+ * binary_tree_height -  measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height.
+ * Return: height of tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_len = 0, right_len = 0;
+
+	if (!tree)
+		return (0);
+	
+	if (tree->left)
+		left_len = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right_len = 1 + binary_tree_height(tree->right);
+
+	if (left_len > right_len)
+		return (left_len);
+	return (right_len);
+}
