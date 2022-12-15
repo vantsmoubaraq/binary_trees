2655907bba7f5f3237eb5099e771f7ec0977efb7
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-12-15 03:38:10 -0800

    binary_trees

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -13,7 +13,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (!tree->right && !tree->left)
 		return (1);
-	return (0);
+	else
+		return (0);
 
 	return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
 }
