7c31945f549931cada044d432b28ba59179353e5
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-12-15 04:11:54 -0800

    binary_trees

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,19 @@
 #include "binary_trees.h"
 
 /**
+ * size - sizee of tree
+ * @tree: pointer to root
+ * Return: size of tree
+ */
+
+int size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (1 + size(tree->left) + size(tree->right));
+}
+
+/**
  * binary_tree_is_perfect -  checks if a binary tree is perfect
  * @tree: pointer to the root node of the tree to check
  * Return: 1 if perfect or 0 otherwisw
@@ -8,13 +21,18 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int size_left, size_right, compare;
+
 	if (!tree)
 		return (0);
 
-	if (!tree->right && !tree->left)
+	size_left = size(tree->left);
+	size_right = size(tree->right);
+
+	compare = size_left - size_right;
+
+	if (!compare && !tree->right && !tree->left)
 		return (1);
 	else
 		return (0);
-
-	return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
 }
